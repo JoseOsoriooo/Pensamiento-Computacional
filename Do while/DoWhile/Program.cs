185b5a7f5c10c710c518fa939640134dc799352f
JoseOsoriooo <osoriojose1504@gmail.com>
2025-05-25 15:14:14 -0600

    Uso del Do while

---
+++ Do while/DoWhile/Program.cs
@@ -0,0 +1,33 @@
+﻿//int numero;
+//do
+//{
+    //Console.WriteLine("Ingrese un numero positivo:");
+    //numero=Convert.ToInt32(Console.ReadLine());
+    //if (numero<=0)
+    //{
+       // Console.WriteLine("Número incorrecto, ingrese de nuevo.");
+    //}
+    //else
+    //{
+       // Console.WriteLine("Número correcto.");
+    //}
+//} while (numero<=0);
+
+int edad;
+do
+{
+    Console.WriteLine("Ingrese su edad:");
+    edad=Convert.ToInt32(Console.ReadLine());
+    if (edad<=0)
+    {
+        Console.WriteLine("Edad incorrecta, ingrese de nuevo.");
+    }
+    else if (edad>100)
+    {
+        Console.WriteLine("Edad incorrecta, ingrese de nuevo.");
+    }
+    else
+    {
+        Console.WriteLine("Edad correcta.");
+    }
+} while (edad<=0 || edad>100);
