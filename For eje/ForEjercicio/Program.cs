dbb9380ec6f6af27f7b567c7dba6ff4d8a880e63
JoseOsoriooo <osoriojose1504@gmail.com>
2025-05-25 15:20:03 -0600

    Ejercicio de For

---
+++ For eje/ForEjercicio/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//Console.WriteLine("Suma de los numeros del 1 al 100");
+
+//int suma=0;
+//for(int actual=1;actual<=100;actual++){
+    //suma= suma + actual;
+//}
+//Console.WriteLine("La suma es:"+ suma);
+
+Console.WriteLine("Ingrese un numero entero");
+
+int multiplicando= int.Parse(Console.ReadLine());
+
+for(int resultado=1;resultado<=10;resultado++){
+    Console.WriteLine(multiplicando+" x "+resultado+" = "+multiplicando*resultado);
+}
+
+
