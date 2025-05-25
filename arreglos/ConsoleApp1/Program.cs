05acbef260746536717c1ed7ea95c64657f1181d
JoseOsoriooo <osoriojose1504@gmail.com>
2025-05-25 15:12:32 -0600

    Arreglos

---
+++ arreglos/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ejemplo de arreglos");
+
+string [] nombres = ["Juan", "Pedro", "Luisa", "Adriana", "Sofia"]; 
+int [] notas = [88, 75, 96, 77, 59];
+int promedio = 0;
+string nombre = nombres[0];
+for (int i = 0; i < nombres.Length; i++)
+{
+    Console.WriteLine($"Nombre: {nombres[i]} - Nota: {notas[i]}");
+     promedio += notas[i];
+    
+    
+}
+    promedio /= notas.Length;
+    Console.WriteLine($"El pormedio de la suma de todas las notas es: {promedio}");
