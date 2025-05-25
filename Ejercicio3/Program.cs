0c8a7e7a157054a8a50f88aa3d8bc363db532f25
JoseOsoriooo <osoriojose1504@gmail.com>
2025-05-25 15:15:39 -0600

    Ejercicio 3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ejercicio2");
+Console.Writeline("Bienvenido, Ingrese sus nombres:");
+string nombres;
+nombres = Console.Readline();
+Console.Writeline("Ahora ingrese sus apellidos:");
+string apellidos;
+apellidos = Console.Readline();
+Console.Writeline("Su nombre completo es:" + nombres + apellidos);
+Console.Writeline("Ahora ingrese su estaatura en metros:");
+double estatura;
+estatura = Convert.toDouble(Console.Readline());
+Console.Writeline("Su estatura es de:" + estatura + "metros"):
+Console.Writeline("Ahora ingrese su edad:");
+int edad;
+edad = Convert.ToInt16(Console.ReadLine());
+Console.Writeline("Su edad es de:" + edad + "anos");
+Console.Writeline("Ahora ingrese la inicial de su mascota:");
+char inicial;
+inicial = Convert.toChar(Console.Readline());
+Console.Writeline("La inicial de su mascota es:" + edad);
