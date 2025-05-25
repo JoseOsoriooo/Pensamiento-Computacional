a548d52cf8ac227b7ae655c122c70f87e4189470
JoseOsoriooo <osoriojose1504@gmail.com>
2025-05-25 15:26:01 -0600

    Ejrcicio de do while

---
+++ dodowhile/eeeje5do/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Ingrese un texto");
+string texto = Console.ReadLine();
+int cantidadDePablabras = texto.Length;
+string textoFormateado = "";
+string[] palabras = texto.Split(' ');
+
+for (int i = 0; i < texto.Length; i++)
+{
+   string palabra = palabras [i];
+    if (palabra == " ")
+    {
+         textoFormateado += " ";
+         textoFomentado += palabras[i].ToUpper() + " ";
+    }
+    
+    
+   }
+
+{
+    Console.WriteLine($"El texto formateado es: {textoFormateado}");
+    Console.WriteLine($"El texto tiene {cantidadDePablabras} palabras");
+    
+    
+    
+    
+
+}
